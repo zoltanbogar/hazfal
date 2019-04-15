2f1bb21b25b8be37ddd65502797a3fc94e7d0301
Tomán Viktor <viktor.toman19@gmail.com>
2019-04-15 19:31:48 +0200

    Fix

--- src/AdminBundle/Controller/BuildingController.php
+++ src/AdminBundle/Controller/BuildingController.php
@@ -129,7 +129,7 @@ class BuildingController extends Controller
                 ];
                 $success = FALSE;
             }
-            i
+            
 
             if (!$error) {
                 $entityManager->persist($objHouse);
